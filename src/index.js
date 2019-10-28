5f0b02f47b33e8611d9b595449a9f6fbb5e2163a
Casey Kelly <casey.kelly@colorado.edu>
2019-10-27 20:10:54 -0600

    add votes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Vote = require('./resolvers/Vote')
 const Subscription = require('./resolvers/Subscription')
 const { prisma } = require('./generated/prisma-client')
 const { GraphQLServer } = require('graphql-yoga')
@@ -12,6 +13,7 @@ const resolvers = {
   User,
   Link,
   Subscription,
+  Vote,
 }
 
 const server = new GraphQLServer({

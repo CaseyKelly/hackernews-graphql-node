6b9221917be338b686db0c86529b058390fe9d50
Casey Kelly <casey.kelly@colorado.edu>
2019-10-27 19:53:51 -0600

    add subscriptions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
 const { prisma } = require('./generated/prisma-client')
 const { GraphQLServer } = require('graphql-yoga')
 
@@ -9,7 +10,8 @@ const resolvers = {
   Query,
   Mutation,
   User,
-  Link
+  Link,
+  Subscription,
 }
 
 const server = new GraphQLServer({
